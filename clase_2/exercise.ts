dff1a7304cb33ee62dd5fae5e0893a75e5fa15e5
Mario Cervantes <marioyeahs@gmail.com>
2021-09-05 22:22:07 -0500

    add: ts config and other scripts

---
+++ clase_2/exercise.ts
@@ -0,0 +1,13 @@
+let variable1 = "123";
+let variable2 = false;
+let variable3 = 23.2;
+let variable4 = "true";
+let variable5 = 3;
+let variable6 = true;
+
+console.log( typeof variable1 == "number")
+console.log( typeof variable2 == "boolean")
+console.log( typeof variable3 == "number")
+console.log( typeof variable4 == "boolean")
+console.log( typeof variable5 == "string")
+console.log( typeof variable6 == "boolean")
---
+++ clase_2/types.ts
@@ -0,0 +1,28 @@
+const number = 20;
+const djisktra = 2^53 ;
+const string = "hello";
+const boolean = true;
+const objectArray = ["item", number, 12.2398, false, [1,2,3], 'a'];
+const object = { key1: "Value", number: 13, "Juan Perez": true };
+const indefinido = undefined;
+const nulo = null;
+console.log(typeof number)
+console.log(typeof djisktra)
+console.log(typeof string)
+console.log(typeof boolean)
+console.log(typeof objectArray)
+console.log(typeof object)
+console.log(typeof indefinido)
+console.log(typeof nulo)
+
+let number1: number;
+let cadena: string;
+let booleano: boolean;
+let arreglo: number[];
+let objetos: {
+    edad: number;
+    nombre: string;
+    genero: boolean;
+}
+
+
